8686c821d98a6a2975101965b9ea91d9d06db6fb
cristian97r <cristian97drb@gmail.com>
2019-04-13 09:43:29 -0400

    add authentication

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,51 +1,61 @@
-import React, { Component } from 'react'
-import { ListGroup, ListGroupItem, Button } from 'reactstrap'
-import {CSSTransition, TransitionGroup} from 'react-transition-group'
-import { connect } from 'react-redux'
-import { getItems, deleteItem } from '../actions/itemsActions'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-  componentDidMount(){
-    this.props.getItems()
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool
+  };
+
+  componentDidMount() {
+    this.props.getItems();
   }
-  OnDeleteClick = (id) => {
-    this.props.deleteItem(id)
-  }
+
+  onDeleteClick = id => {
+    this.props.deleteItem(id);
+  };
 
   render() {
-
-    const { items } = this.props.item
-
+    const { items } = this.props.item;
     return (
-          <ListGroup>
-            <TransitionGroup className="shopping-list">
-              {items.map(({ _id, name }) => (
-                <CSSTransition key={_id} timeout={500} classNames="fade">
-                  <ListGroupItem>
+      <Container>
+        <ListGroup>
+          <TransitionGroup className='shopping-list'>
+            {items.map(({ _id, name }) => (
+              <CSSTransition key={_id} timeout={500} classNames='fade'>
+                <ListGroupItem>
+                  {this.props.isAuthenticated ? (
                     <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.OnDeleteClick.bind(this, _id)}
-                    >&times;</Button>
-                    {name}
-                  </ListGroupItem>
-                </CSSTransition>
-              ))}
-            </TransitionGroup>
-          </ListGroup>
-    )
+                      className='remove-btn'
+                      color='danger'
+                      size='sm'
+                      onClick={this.onDeleteClick.bind(this, _id)}
+                    >
+                      &times;
+                    </Button>
+                  ) : null}
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
   }
 }
 
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired
-}
+const mapStateToProps = state => ({
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
+});
 
-const mapStateToProps = (state) => ({
-  item: state.item
-})
-
-export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList)
+export default connect(
+  mapStateToProps,
+  { getItems, deleteItem }
+)(ShoppingList);
--- models/Item.js
+++ models/Item.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+// Create Schema
 const ItemSchema = new Schema({
-    name: {
-        type: String, 
-        require: true
-    },
-    date: {
-        type: Date, 
-        default: Date.now
-    } 
-})
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports = Item = mongoose.model('item', ItemSchema)
+module.exports = Item = mongoose.model('item', ItemSchema);
